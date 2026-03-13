41eaf37bb5148973be6d4417baf09165ef72d1e0
Pranotosh Mandal <mandalprano@gmail.com>
2026-03-14 01:26:27 +0530

    added authorizations

---
+++ app/api/routes_auth.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter
+
+from pydantic import BaseModel
+
+from app.core.security import create_token
+
+router = APIRouter()
+
+class AuthInput(BaseModel):
+    username:str
+    password:str
+    
+@router.post("/login")
+def login(auth:AuthInput):
+    if auth.username=="admin" and auth.password=="password":
+        token=create_token({"sub":auth.username})
+        return {"access_token":token}
+    else:
+        return {"detail":"Invalid credentials"}    
+
